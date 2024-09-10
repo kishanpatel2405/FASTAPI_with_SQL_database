f0884aabeb8e32bb225415fec033ecee7e7fd004
kishan vaghasiya <145438858+kishanpatel2405@users.noreply.github.com>
2024-09-10 12:43:40 +0530

    Update database.py

--- FASTAPI_with_SQL_database/database.py
+++ FASTAPI_with_SQL_database/database.py
@@ -6,3 +6,7 @@ DATABASE_URL = "sqlite:///./user.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+
+
